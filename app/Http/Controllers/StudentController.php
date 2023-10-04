ed4c3f8c73333167661c0ae298727a082148a640
kdungx <dungkath2209058@fpt.edu.vn>
2023-10-04 16:23:11 +0700

    commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,7 +10,7 @@ class StudentController extends Controller
 {
     public function index()
     {
-        $students = DB::table();
+        $students = DB::table("students")->get();
         return view('students.index', compact('students'));
     }
 
